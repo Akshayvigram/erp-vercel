873a92f86e30cbbe64c7bccd8f5adbe43ef0cf8a
MouleeswaranR <ffmouleesh@gmail.com>
2025-04-19 12:38:24 +0530

    changes

--- server/db/envVars.js
+++ server/db/envVars.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const ENV_VARS={
     MONGODB_URL:process.env.MONGODB_URL,
     PORT:process.env.PORT||5000,
-
+    NODE_ENV:process.env.NODE_ENV,
     JWT_KEY:process.env.JWT_KEY,
    
 }
